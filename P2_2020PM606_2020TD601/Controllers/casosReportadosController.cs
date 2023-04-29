a21c2ea80db000e03fe7fc993a788e3186719302
Alejandro Toledo <88223531+WernerToledo@users.noreply.github.com>
2023-04-29 13:04:17 -0600

    Trabajando en la interfaz

---
+++ P2_2020PM606_2020TD601/Controllers/casosReportadosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P2_2020PM606_2020TD601.Controllers
+{
+    public class casosReportadosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
