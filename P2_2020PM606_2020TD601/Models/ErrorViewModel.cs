3651b704961a60c264fa1baa55d79dd93ac556ed
Alejandro Toledo <88223531+WernerToledo@users.noreply.github.com>
2023-04-29 12:21:59 -0600

    Agregar archivos de proyecto.

---
+++ P2_2020PM606_2020TD601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P2_2020PM606_2020TD601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
